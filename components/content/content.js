be14d0b05624a81f5fe2815c7c07995e2df31b9d
Berta Górriz <bertagorrizfernandez@gmail.com>
2025-02-02 20:18:29 +0100

    feat: add `variety` section

--- components/content/content.js
+++ components/content/content.js
@@ -1,3 +1,5 @@
+import { map } from "../functions/functions.js";
+
 gsap.registerPlugin(SplitText, ScrollTrigger);
 
 // HERO
@@ -43,6 +45,75 @@ const createSplit = () => {
 };
 
 createSplit();
+
+// VARIETY
+const texts = ["Dark", "Light", "Navy", "Denim"];
+
+const createBands = () => {
+  const wrapper = document.getElementById("variety-section");
+
+  texts.forEach((text) => {
+    const bandWrapper = document.createElement("div");
+    bandWrapper.classList.add("band-wrapper");
+
+    const band = document.createElement("div");
+    band.classList.add("band");
+
+    const p = document.createElement("p");
+    p.textContent = text;
+
+    band.appendChild(p);
+    bandWrapper.appendChild(band);
+    wrapper.appendChild(bandWrapper);
+
+    positionBand(bandWrapper);
+  });
+};
+
+const positionBand = (band) => {
+  const width = window.innerWidth * 0.2;
+  const height = window.innerHeight * 0.2;
+
+  const x = map(Math.random(), 0, 1, -width, width);
+  const y = map(Math.random(), 0, 1, -height, height);
+  const rotation = Math.random() * 360;
+
+  gsap.set(band, {
+    x,
+    y,
+    rotation,
+  });
+};
+
+const animateBands = () => {
+  gsap.set(".band", {
+    xPercent: 100,
+  });
+
+  const tl = gsap.timeline({
+    paused: true,
+  });
+
+  tl.to(".band", {
+    xPercent: -100,
+    duration: 8,
+    ease: "power4.inOut",
+    stagger: 0.05,
+    scrollTrigger: {
+      trigger: "#variety-section",
+      start: "top 80%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+  });
+};
+
+const initBands = () => {
+  createBands();
+  animateBands();
+};
+
+initBands();
 
 export const initContent = () => {
   console.log("Content initialized");
